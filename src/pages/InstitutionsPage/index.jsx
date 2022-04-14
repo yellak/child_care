603e98296cbd8703361063e6fd1cddfda0896451
Felipe <felipevaz444@gmail.com>
2022-04-13 21:52:38 -0300

    :sparkles: Tabela e pagina criada

--- src/pages/InstitutionsPage/index.jsx
+++ src/pages/InstitutionsPage/index.jsx
@@ -1,9 +1,39 @@
 import { Container } from './styles'
+import image from '../../assets/foto.png'
 
 const InstitutionsPage = () => {
   return(
     <Container>
-      <h1> Ola cornos </h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Instituição</th>
+            <th>Arrecadação</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src={image} alt="" />
+            </td>
+            <td>Instituição Criança Top</td>
+            <td>
+              <p> R$ 3.500,00/ R$ 4.0000,00 </p>
+              <div className='progress-bar'>
+                <div className='progress'></div>
+                <div className='not-progress'></div>
+              </div>
+            </td>
+            <td>
+              <button>
+                Detalhes
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </Container>
   )
 }
--- src/pages/InstitutionsPage/styles.jsx
+++ src/pages/InstitutionsPage/styles.jsx
@@ -1,5 +1,77 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  color: red;
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  justify-content: center;
+  margin: 5vh 0;
+  
+  table {
+    text-align: center;
+    width: 80vw;
+    border-collapse: collapse;
+  }
+
+  th {
+    font-size: 25px;
+    font-weight: 100;
+    padding-bottom: 2vh;
+  }
+
+  td {
+    border-bottom: 1px solid;
+    padding: 0 10px;
+    font-size: 20px;
+  }
+
+  tbody{
+    border: 1px solid;
+  }
+
+  img {
+    padding: 10px;
+    height: 100%;
+  }
+
+  p {
+    margin: 0 0 5px 0;
+  }
+
+  button {
+    width: 100%;
+    font-size: 18px;
+    padding: 10px 30px;
+    border: none;
+    border-radius: 7px;
+    background: #62CD50;
+    transition: 300ms;
+    cursor: pointer;
+  }
+
+  button:hover {
+    transition: 300ms;
+    filter: brightness(0.7)
+  }
+
+  .progress-bar {
+    height: 10px;
+    width: 100%;
+    display: flex;
+
+    .progress {
+      background-color: #D2BD01;
+      width: 70%;
+      height: 100%;
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+    }
+
+    .not-progress {
+      background-color: #BBBFBB;
+      width: 30%;
+      height: 100%;
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
+    }
+  }
 `

8b80695707ba5ef70e8a6c5fad24ffea331c7985
André Macedo <andremacedo.pv@gmail.com>
2022-04-17 10:42:07 -0300

    :sparkles: Donation first page

--- src/pages/MakeDonationPage/index.jsx
+++ src/pages/MakeDonationPage/index.jsx
@@ -1,17 +1,84 @@
-import { Container, Header, BoxContainer } from './styles'
-import InstitutionRow from '../../components/InstitutionRow'
-import image from '../../assets/foto.png'
+import { Container, 
+         Header, 
+         BoxContainer, 
+         BoxHeader, 
+         BoxBody, 
+         CircleDiv, 
+         ToggleBtn, 
+         ToggleContainer,
+         ValueOptions,
+         ValueButton,
+         ValueInput,
+         MessageBox,
+         InfoText,
+         MessageInput,
+         SubmitButton } from './styles'
+import { BsCircle, BsCircleFill } from 'react-icons/bs';
+import { useState } from 'react';
 
 const MakeDonationPage = () => {
+
+  const [page, setPage] = useState(1);
+  const [frequency, setFrequency] = useState('once');
+  const [value, setValue] = useState(0);
+  const [personalized, setPersonalized] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const handleValueChange = (e) => {
+    setValue(e.value)
+    setPersonalized(true)
+  }
+
+  const handleValueButton = (v) => {
+    setValue(v)
+    setPersonalized(false)
+  }
+
+  const handleFrequency = (v) => {
+    setFrequency(v)
+  }
+
+  const donationPage = () => {
+    return(
+      <BoxBody>
+          <ToggleContainer>
+            <ToggleBtn side={"left"} checked={frequency === 'once'} onClick={() => handleFrequency('once')}>Uma Vez</ToggleBtn>
+            <ToggleBtn checked={frequency === 'monthly'} onClick={() => handleFrequency('monthly')}>Mensal</ToggleBtn>
+          </ToggleContainer>
+          <ValueOptions>
+            <ValueButton checked={!personalized && (value === 50)} onClick={() => handleValueButton(50)}>R$ 50,00</ValueButton>
+            <ValueButton checked={!personalized && (value === 100)} onClick={() => handleValueButton(100)}>R$ 100,00</ValueButton>
+            <ValueButton checked={!personalized && (value === 150)} onClick={() => handleValueButton(150)}>R$ 150,00</ValueButton>
+            <ValueButton checked={!personalized && (value === 250)} onClick={() => handleValueButton(250)}>R$ 250,00</ValueButton>
+            <ValueButton checked={!personalized && (value === 500)} onClick={() => handleValueButton(500)}>R$ 500,00</ValueButton>
+            <ValueButton checked={!personalized && (value === 800)} onClick={() => handleValueButton(800)}>R$ 800,00</ValueButton>
+          </ValueOptions>
+          <ValueInput type="number" step="0.01" placeholder='Valor Personalizado' value={personalized? value: ''} onChange={handleValueChange}></ValueInput>
+          <MessageBox>
+            <InfoText>Enviar mensagem com a doação</InfoText>
+            <MessageInput placeholder='Digite sua mensagem aqui' onChange={(e) => {setMessage(e.message)}}></MessageInput>
+          </MessageBox>
+          <SubmitButton onClick={() => {setPage(2)}}>Continuar</SubmitButton>
+      </BoxBody>
+    )
+  }
 
   return(
     <Container>
       <Header>
-        <h1>Instituição Criança Top</h1>
-        <h2>Nome da Criança</h2>
+        <span>Instituição Criança Top</span>
+        <span>Nome da Criança</span>
       </Header>
       <BoxContainer>
-
+        <BoxHeader>
+          <span>Escolher valor da doação</span>
+          <CircleDiv>
+            <BsCircleFill style={{marginLeft: 1}}/>
+            <BsCircle />
+            <BsCircle />
+          </CircleDiv>
+        </BoxHeader>
+        {page === 1? donationPage() : ''}
       </BoxContainer>
     </Container>
   )
--- src/pages/MakeDonationPage/styles.jsx
+++ src/pages/MakeDonationPage/styles.jsx
@@ -3,91 +3,152 @@ import styled from 'styled-components'
 export const Container = styled.div`
   font-family: "Roboto", sans-serif;
   display: flex;
-  justify-content: center;
-  margin: 5vh 0;
-  
-  table {
-    text-align: center;
-    width: 80vw;
-    border-collapse: collapse;
-  }
-
-  th {
-    font-size: 25px;
-    font-weight: 100;
-    padding-bottom: 2vh;
-  }
-
-  tbody{
-    border: 1px solid;
-  }
+  flex-direction: column;
+  align-items: center;
+  min-height: 100%;
 `
 
 export const Header = styled.div`
   font-family: "Roboto", sans-serif;
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
-  margin: 5vh 0;
-  
-  table {
-    text-align: center;
-    width: 80vw;
-    border-collapse: collapse;
-  }
+  margin-bottom: 1rem;
+  margin-top: 1rem;
 
-  th {
-    font-size: 25px;
-    font-weight: 100;
-    padding-bottom: 2vh;
-  }
-
-  tbody{
-    border: 1px solid;
+  span{
+    font-size: 2rem;
+    font-weight: 200;
   }
 `
 
 export const BoxContainer = styled.div`
   font-family: "Roboto", sans-serif;
   display: flex;
-  justify-content: center;
-  margin: 5vh 0;
-  
-  table {
-    text-align: center;
-    width: 80vw;
-    border-collapse: collapse;
+  flex-direction: column;
+  align-items: center;
+  height: 63vh;
+  width: 40%;
+  @media (max-width: 1000px)
+  {
+    width: 70%;
   }
-
-  th {
-    font-size: 25px;
-    font-weight: 100;
-    padding-bottom: 2vh;
-  }
-
-  tbody{
-    border: 1px solid;
-  }
+  background: rgba(15, 144, 152, 0.39);
+  border-radius: 26px;
 `
 
 export const BoxHeader = styled.div`
   font-family: "Roboto", sans-serif;
   display: flex;
-  justify-content: center;
-  margin: 5vh 0;
-  
-  table {
-    text-align: center;
-    width: 80vw;
-    border-collapse: collapse;
-  }
+  align-items: center;
+  flex-direction: column;
+  padding-top: 1.5rem;
+  padding-bottom: 1.1rem;
+  border-bottom: 1px solid;
+  width: 100%;
 
-  th {
-    font-size: 25px;
-    font-weight: 100;
-    padding-bottom: 2vh;
-  }
-
-  tbody{
-    border: 1px solid;
+  span{
+    font-size: 1.25rem;
   }
 `
+export const CircleDiv = styled.div`
+  display: flex;
+  margin-top: 0.5rem;
+  gap: 0.4rem;
+  cursor: pointer;
+`;
+
+export const BoxBody = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const ToggleBtn = styled.button`
+  width: 5rem;
+  height: 2rem;
+  background-color: ${props => (props.checked? '#F1F1F1' : 'transparent')};
+  border: 0.5px solid #000; 
+  border-radius: ${props => (props.side === 'left'? '10px 0 0 10px' : '0 10px 10px 0')};
+
+  &:hover{
+    background-color: #E2E2E2;
+  }
+`;
+
+export const ToggleContainer = styled.div`
+  margin-top: 2vh;
+  margin-bottom: 2vh;
+`;
+
+export const ValueOptions = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  flex-direction: row;
+  gap: 1rem;
+  width: 65%;
+`;
+
+export const ValueButton = styled.button`
+  width: 7rem;
+  height: 4rem;
+  font-size: 1.2rem;
+  background-color: ${props => (props.checked? '#F1F1F1' : 'transparent')};
+  border: 0.5px solid #000; 
+
+  &:hover{
+    background-color: #E2E2E2;
+  }
+`;
+
+export const ValueInput = styled.input`
+  height: 3rem;
+  margin-top: 1rem;
+  font-size: 1.2rem;
+  width: 21.7rem;
+  border: 0.5px solid #000; 
+  background-color: transparent;
+  padding-left: 1rem;
+  padding-rigth: 1rem;
+`;
+
+export const MessageInput = styled.textarea`
+  font-size: 1rem;
+  font-family: "Roboto", sans-serif;
+  height: 5rem;
+  padding-top: 0.25rem;
+  margin-top: 0.5rem;
+  width: 22.4rem;
+  border: 0.5px solid #000; 
+  background-color: transparent;
+  padding-left: 0.5rem;
+  padding-rigth: 0.5rem;
+`;
+
+export const InfoText = styled.span`
+
+`;
+
+export const MessageBox = styled.div`
+  display: flex;
+  margin-top: 2rem;
+  flex-direction: column;
+`;
+
+export const SubmitButton = styled.button`
+  margin-top: 1rem;
+  width: 23.4rem;
+  height: 2.7rem;
+  color: #F1F1F1;
+  font-size: 1rem;
+  border: none;
+  background: linear-gradient(90.96deg, rgba(48, 5, 140, 0.84) 0%, rgba(9, 34, 98, 0.84) 100%);
+  border-radius: 8px;
+  cursor: pointer;
+
+  &:hover{
+    background: linear-gradient(10deg, rgba(48, 5, 140, 0.84) 0%, rgba(9, 34, 98, 0.84) 100%);
+  }
+`;
